b0995215f67036ad4365e833b0efafd376f0c3d5
Scott Hermes <scotthermes@hotmail.com>
2016-04-03 16:33:46 -0500

    Initial

---
+++ Chat/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(Chat.Startup))]
+
+namespace Chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            //app.MapSignalR<MyEndPoint>("/echo");
+        }
+    }
+}
